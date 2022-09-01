c6c0a515d53bf8272ed8553dac44c42c44683015
Egorova Tatiana <tanyasha.89@mail.ru>
2022-09-01 20:41:42 +0300

    seminars8homework task58

--- Program.cs
+++ Program.cs
@@ -53,7 +53,7 @@ void ShowArray(int[,] array)
     }
 }
 */
-
+/*
 //Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
 
 Console.WriteLine("array squared: ");
@@ -102,3 +102,60 @@ void ShowArray(int[,] array)
         Console.WriteLine("");
     }
 }
+*/
+/*
+//Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 100);
+        }
+    }
+}
+
+void ShowArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine("");
+    }
+}
+
+int size = InputInt("matrix size: ");
+int[,] matrixA = new int[size, size];
+int[,] matrixB = new int[size, size];
+FillArrayRandomNumbers(matrixA);
+FillArrayRandomNumbers(matrixB);
+int[,] matrixC = new int[size, size];
+
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        for (int k = 0; k < size; k++)
+        {
+            matrixC[i, j] = matrixC[i, j] + (matrixA[i, k] * matrixB[k, j]);
+        }
+    }
+}
+Console.WriteLine("MatrixА");
+ShowArray(matrixA);
+Console.WriteLine("MatrixВ");
+ShowArray(matrixB);
+Console.WriteLine("Matrix А*В");
+ShowArray(matrixC);
+*/
