048939919f0637c7cce681af7c76a9321335e439
Egorova Tatiana <tanyasha.89@mail.ru>
2022-09-01 20:11:37 +0300

    seminars8homework task54

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿
+// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Console.WriteLine("number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("number of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[rows, columns];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine();
+Console.WriteLine("Array: ");
+ShowArray(numbers);
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1) - 1; j++)
+    {
+        for (int z = 0; z < numbers.GetLength(1) - 1; z++)
+        {
+            if (numbers[i, z] < numbers[i, z + 1])
+            {
+                int temp = 0;
+                temp = numbers[i, z];
+                numbers[i, z] = numbers[i, z + 1];
+                numbers[i, z + 1] = temp;
+            }
+        }
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Array total: ");
+ShowArray(numbers);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 100);
+        }
+    }
+}
+
+void ShowArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine("");
+    }
+}
