06cd1d5d3b043fe75d0aae92388dbd0dbe613ba2
Egorova Tatiana <tanyasha.89@mail.ru>
2022-09-01 20:24:15 +0300

    seminars8homework task56

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿/*
 // Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
 Console.WriteLine("number of rows: ");
 int rows = Convert.ToInt32(Console.ReadLine());
@@ -29,6 +29,56 @@ for (int i = 0; i < numbers.GetLength(0); i++)
 Console.WriteLine();
 Console.WriteLine("Array total: ");
 ShowArray(numbers);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 100);
+        }
+    }
+}
+
+void ShowArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine("");
+    }
+}
+*/
+
+//Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Console.WriteLine("array squared: ");
+int massVol = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[massVol,massVol];
+FillArrayRandomNumbers(numbers);
+ShowArray(numbers);
+int minsum = Int32.MaxValue;
+int indexLine = 0;
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    int sum = 0;
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        sum = sum + numbers[i, j];        
+    }
+    if (sum < minsum)
+    {
+        minsum = sum;
+        indexLine++;
+    }
+}
+
+Console.WriteLine("row with the smallest amount of elements under the number: " + (indexLine) + "sum of elements: " + (minsum));
 
 void FillArrayRandomNumbers(int[,] array)
 {
